3f44596505d8b5e8c3de21bd47704b41613a6c33
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2025-02-07 23:09:23 +0530

    wip: agent

--- packages/ai-agent-sdk/src/core/agent/agent.ts
+++ packages/ai-agent-sdk/src/core/agent/agent.ts
@@ -7,6 +7,43 @@ import type { ParsedFunctionToolCall } from "openai/resources/beta/chat/completi
 import type { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 import z, { type AnyZodObject } from "zod";
 
+export class Agent extends Base {
+    private config: AgentConfig;
+    private llm: LLM;
+    private _tools: Record<AgentName, Tool>;
+    constructor(config: AgentConfig) {
+        super("agent");
+        this.config = config;
+        this.llm = new LLM();
+        this._tools = config.tools || {};
+    }
+
+    get description() {
+        return this.config.description;
+    }
+
+    get instructions() {
+        return this.config.instructions;
+    }
+
+    get tools(): Record<AgentName, Tool> {
+        return this._tools;
+    }
+
+    async generate<T extends Record<string, AnyZodObject>>(
+        messages: ChatCompletionMessageParam[],
+        response_schema: T
+    ) {
+        return this.llm.generate(messages, response_schema, this.tools);
+    }
+
+    async run(state: ZeeWorkflowState = StateFn.root(this.description)) {
+        return this.config.runFn
+            ? this.config.runFn(this, state)
+            : defaultFn(this, state);
+    }
+}
+
 const getSteps = (conversation: ChatCompletionMessageParam[]) => {
     const messagePairs = conversation.reduce(
         (
@@ -249,85 +286,3 @@ export const resource_planner = (agents: Record<AgentName, Agent>) =>
             return StateFn.passdown(state, result.value.agent);
         },
     });
-
-export const endgame = () =>
-    new Agent({
-        name: "endgame",
-        description: "",
-        model: {
-            provider: "OPEN_AI",
-            name: "gpt-4o-mini",
-        },
-
-        runFn: async (agent: Agent, state) => {
-            const messages = [
-                    system("maxIterations limit hit"),
-                    user(
-                        "Please summarize all previously executed steps and do your best to achieve the main goal while responding with the final answer."
-                    ),
-                ],
-                schema = {
-                    task_result: z.object({
-                        final_answer: z
-                            .string()
-                            .describe("The final result of the task"),
-                    }),
-                };
-
-            const result = await agent.generate(messages, schema);
-            if (!("final_answer" in result.value)) {
-                return StateFn.finish(
-                    state,
-                    assistant("Failed to get final answer")
-                );
-            }
-
-            if (result.type !== "task_result") {
-                throw new Error(
-                    "Expected task_result response, got " + result.type
-                );
-            }
-
-            return StateFn.finish(
-                state,
-                assistant(result.value["final_answer"])
-            );
-        },
-    });
-
-export class Agent extends Base {
-    private config: AgentConfig;
-    private llm: LLM;
-    private _tools: Record<AgentName, Tool>;
-    constructor(config: AgentConfig) {
-        super("agent");
-        this.config = config;
-        this.llm = new LLM(config.model);
-        this._tools = config.tools || {};
-    }
-
-    get description() {
-        return this.config.description;
-    }
-
-    get instructions() {
-        return this.config.instructions;
-    }
-
-    get tools(): Record<AgentName, Tool> {
-        return this._tools;
-    }
-
-    async generate<T extends Record<string, AnyZodObject>>(
-        messages: ChatCompletionMessageParam[],
-        response_schema: T
-    ) {
-        return this.llm.generate(messages, response_schema, this.tools);
-    }
-
-    async run(state: ZeeWorkflowState = StateFn.root(this.description)) {
-        return this.config.runFn
-            ? this.config.runFn(this, state)
-            : defaultFn(this, state);
-    }
-}
